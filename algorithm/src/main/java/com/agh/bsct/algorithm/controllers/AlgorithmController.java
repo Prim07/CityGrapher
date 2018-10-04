06469a69c341b9205d0661e8d92a13699c1b4c9e
Artur <kawala.artek@gmail.com>
2018-10-04 20:24:09 +0200

    Add example endpoint for algorithm

---
+++ algorithm/src/main/java/com/agh/bsct/algorithm/controllers/AlgorithmController.java
@@ -0,0 +1,17 @@
+package com.agh.bsct.algorithm.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class AlgorithmController {
+
+    private static final String ALGORITHM_PATH = "algorithm/";
+
+    @RequestMapping(method = RequestMethod.GET, value = ALGORITHM_PATH)
+    public String getResults() {
+        return "Hello, Algorithm!";
+    }
+
+}
