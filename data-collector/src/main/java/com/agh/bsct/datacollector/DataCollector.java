990c2aaa692feb3f9a428e29b5b0a6deb549688b
Artur <kawala.artek@gmail.com>
2018-10-06 20:39:24 +0200

    Add main package and DataCollector class

---
+++ data-collector/src/main/java/com/agh/bsct/datacollector/DataCollector.java
@@ -0,0 +1,16 @@
+package com.agh.bsct.datacollector;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+//TODO AK restrict paths when comes more packages
+@ComponentScan({"com.agh.bsct.datacollector"})
+@EntityScan({"com.agh.bsct.datacollector"})
+public class DataCollector {
+    public static void main(String[] args) {
+        SpringApplication.run(DataCollector.class, args);
+    }
+}
