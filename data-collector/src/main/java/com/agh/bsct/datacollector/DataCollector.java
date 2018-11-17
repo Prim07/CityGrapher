10c49b1fcbb27a7a216dfa8ab3cddc43dbc69d4c
Artur <kawala.artek@gmail.com>
2018-11-17 16:48:47 +0100

    Remove @EntityScan as we do not have no Spring entities yet

--- data-collector/src/main/java/com/agh/bsct/datacollector/DataCollector.java
+++ data-collector/src/main/java/com/agh/bsct/datacollector/DataCollector.java
@@ -2,13 +2,10 @@ package com.agh.bsct.datacollector;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @ComponentScan({"com.agh.bsct.datacollector.controllers", "com.agh.bsct.datacollector.services"})
-//TODO AK restrict paths when comes more packages
-@EntityScan({"com.agh.bsct.datacollector"})
 public class DataCollector {
     public static void main(String[] args) {
         SpringApplication.run(DataCollector.class, args);
