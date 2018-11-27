f980ee30acf174a82fd509b8e8279b811d02d734
Artur <kawala.artek@gmail.com>
2018-11-27 20:54:56 +0100

    Change main endpoint method to POST. Add new endpoint for getting info about results or results.

--- algorithm/src/main/java/com/agh/bsct/algorithm/controllers/AlgorithmController.java
+++ algorithm/src/main/java/com/agh/bsct/algorithm/controllers/AlgorithmController.java
@@ -1,9 +1,6 @@
 package com.agh.bsct.algorithm.controllers;
 
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @CrossOrigin
 @RestController
@@ -11,9 +8,15 @@ public class AlgorithmController {
 
     private static final String ALGORITHM_PATH = "algorithm/";
 
-    @RequestMapping(method = RequestMethod.GET, value = ALGORITHM_PATH)
+    @RequestMapping(method = RequestMethod.POST, value = ALGORITHM_PATH)
     public String getResults() {
         return "Hello, Algorithm!";
+    }
+
+    @RequestMapping(method = RequestMethod.GET, value = ALGORITHM_PATH + "{id}")
+    @ResponseBody
+    public String run(@PathVariable("id") String id) {
+        return "Hello, Algorithm! " + id;
     }
 
 }
