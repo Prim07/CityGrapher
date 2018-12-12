08ed3a33f5adaf181bdedbb7503d212d5c31a425
Artur <kawala.artek@gmail.com>
2018-12-12 21:05:45 +0100

    Simple refactor AsyncAlgorithmTaskRunner

--- algorithm/src/main/java/com/agh/bsct/algorithm/services/runner/AsyncAlgorithmTaskRunner.java
+++ algorithm/src/main/java/com/agh/bsct/algorithm/services/runner/AsyncAlgorithmTaskRunner.java
@@ -1,7 +1,8 @@
 package com.agh.bsct.algorithm.services.runner;
 
 import com.agh.bsct.algorithm.Algorithm;
-import com.agh.bsct.api.entities.algorithmresult.AlgorithmResultDTO;
+import com.agh.bsct.algorithm.controllers.mapper.AlgorithmTaskMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Component;
 
@@ -11,16 +12,23 @@ import java.util.Collections;
 public class AsyncAlgorithmTaskRunner {
 
 
+    private AlgorithmTaskMapper algorithmTaskMapper;
+
+    @Autowired
+    public AsyncAlgorithmTaskRunner(AlgorithmTaskMapper algorithmTaskMapper) {
+        this.algorithmTaskMapper = algorithmTaskMapper;
+    }
+
     @Async(Algorithm.SPRING_THREAD_POOL_NAME)
     public void run(AlgorithmTask algorithmTask) {
         //TODO implement here calculating and logic and remove below fake log calculations
         try {
             algorithmTask.setStatus(AlgorithmCalculationStatus.CALCULATING);
             Thread.sleep(2000);
-            AlgorithmResultDTO fakeAlgorithmResult = new AlgorithmResultDTO(algorithmTask.getId(),
-                    algorithmTask.getGraphDataDTO(), Collections.emptyList());
-            algorithmTask.setAlgorithmResultDTO(fakeAlgorithmResult);
             algorithmTask.setStatus(AlgorithmCalculationStatus.SUCCESS);
+
+            var fakeAlgorithmResult = algorithmTaskMapper.mapToAlgorithmResultDTO(algorithmTask, Collections.emptyList());
+            algorithmTask.setAlgorithmResultDTO(fakeAlgorithmResult);
         } catch (InterruptedException e) {
             e.printStackTrace();
             algorithmTask.setStatus(AlgorithmCalculationStatus.FAILED);
