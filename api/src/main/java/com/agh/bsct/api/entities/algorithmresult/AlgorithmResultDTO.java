6458cc24d77825b365be940696023ab843d72ffe
Artur <kawala.artek@gmail.com>
2018-12-12 20:39:15 +0100

    Fix AlgorithmResultDTO to be fully "Data Transfer Objects" :)

--- api/src/main/java/com/agh/bsct/api/entities/algorithmresult/AlgorithmResultDTO.java
+++ api/src/main/java/com/agh/bsct/api/entities/algorithmresult/AlgorithmResultDTO.java
@@ -1,32 +1,26 @@
 package com.agh.bsct.api.entities.algorithmresult;
 
 import com.agh.bsct.api.entities.graphdata.GraphDataDTO;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
+import javax.validation.constraints.NotNull;
 import java.util.List;
 
+@Getter
+@Setter
+@NoArgsConstructor
 public class AlgorithmResultDTO {
 
+    @NotNull
     private GraphDataDTO graphDataDTO;
+
+    @NotNull
     private List<Integer> hospitalIds;
 
     public AlgorithmResultDTO(GraphDataDTO graphDataDTO, List<Integer> hospitalIds) {
         this.graphDataDTO = graphDataDTO;
-        this.hospitalIds = hospitalIds;
-    }
-
-    public GraphDataDTO getGraphDataDTO() {
-        return graphDataDTO;
-    }
-
-    public void setGraphDataDTO(GraphDataDTO graphDataDTO) {
-        this.graphDataDTO = graphDataDTO;
-    }
-
-    public List<Integer> getHospitalIds() {
-        return hospitalIds;
-    }
-
-    public void setHospitalIds(List<Integer> hospitalIds) {
         this.hospitalIds = hospitalIds;
     }
 }
