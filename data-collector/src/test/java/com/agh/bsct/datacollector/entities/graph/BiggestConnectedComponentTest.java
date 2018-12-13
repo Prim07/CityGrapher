d17fc75967eec82da5bc223572c6220d6d640f30
ppaula <palcowskapaula@gmail.com>
2018-12-13 18:58:31 +0100

    COnvert LOOP_COUNT to local variable, extract root resources path to static field

--- data-collector/src/test/java/com/agh/bsct/datacollector/entities/graph/BiggestConnectedComponentTest.java
+++ data-collector/src/test/java/com/agh/bsct/datacollector/entities/graph/BiggestConnectedComponentTest.java
@@ -6,26 +6,27 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class BiggestConnectedComponentTest {
 
-    private final int LOOP_COUNT = 1000;
+    private static final String SRC_TEST_RESOURCES_PATH = "src/test/resources/";
+
     private final GraphInitializer graphInitializer = new GraphInitializer();
 
     @Test
     void shouldBeTheSameWhenMilocinIsCalculatedMultipleTimes() {
-        final String filename = "src/test/resources/milocin.txt";
+        final String filename = SRC_TEST_RESOURCES_PATH + "milocin.txt";
 
         shouldBeTheSameWhenCalculatedMultipleTimes(filename);
     }
 
     @Test
     void shouldBeTheSameWhenLancutIsCalculatedMultipleTimes() {
-        final String filename = "src/test/resources/lancut.txt";
+        final String filename = SRC_TEST_RESOURCES_PATH + "lancut.txt";
 
         shouldBeTheSameWhenCalculatedMultipleTimes(filename);
     }
 
     @Test
     void shouldBeTheSameWhenTarnowIsCalculatedMultipleTimes() {
-        final String filename = "src/test/resources/tarnow.txt";
+        final String filename = SRC_TEST_RESOURCES_PATH + "tarnow.txt";
 
         shouldBeTheSameWhenCalculatedMultipleTimes(filename);
     }
@@ -35,7 +36,8 @@ class BiggestConnectedComponentTest {
 
         var biggestCommonComponentToCompare = graph.getBiggestConnectedComponent();
 
-        for (int i = 0; i < LOOP_COUNT; i++) {
+        int loopCount = 1000;
+        for (int i = 0; i < loopCount; i++) {
             var biggestCommonComponent = graph.getBiggestConnectedComponent();
 
             assertEquals(biggestCommonComponentToCompare.size(), biggestCommonComponent.size());
