530f530d51335134126b734fe0ca5f9c397073ec
ppaula <palcowskapaula@gmail.com>
2018-12-13 19:27:36 +0100

    Remove unnecessary comments

--- data-collector/src/main/java/com/agh/bsct/datacollector/DataCollector.java
+++ data-collector/src/main/java/com/agh/bsct/datacollector/DataCollector.java
@@ -1,16 +1,5 @@
 package com.agh.bsct.datacollector;
 
-import com.agh.bsct.datacollector.controllers.DataCollectorController;
-import com.agh.bsct.datacollector.services.algorithm.boundary.AlgorithmService;
-import com.agh.bsct.datacollector.services.city.OSMCityService;
-import com.agh.bsct.datacollector.services.city.QueryForCityProvider;
-import com.agh.bsct.datacollector.services.data.CityDataService;
-import com.agh.bsct.datacollector.services.data.GraphDataService;
-import com.agh.bsct.datacollector.services.interpreter.QueryInterpreterService;
-import com.agh.bsct.datacollector.services.parser.DataParser;
-import com.agh.bsct.datacollector.services.result.filter.ResultFilterService;
-import com.agh.bsct.datacollector.services.result.joiner.StreetsJoinerService;
-import com.fasterxml.jackson.databind.node.ObjectNode;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
@@ -20,9 +9,6 @@ import org.springframework.context.annotation.ComponentScan;
 public class DataCollector {
     public static void main(String[] args) {
         SpringApplication.run(DataCollector.class, args);
-
-        //DataCollectorController dataCollectorController = new DataCollectorController(new OSMCityService(new GraphDataService(), new CityDataService(new QueryForCityProvider(), new QueryInterpreterService(), new ResultFilterService(), new StreetsJoinerService()), new AlgorithmService(), new DataParser()));
-        //ObjectNode cityData = dataCollectorController.getCityGraph("Bochnia");
     }
 }
 
