9936f774623cc097ba5bee8fdfb1ed3e46af5ea2
ppaula <palcowskapaula@gmail.com>
2018-12-13 18:46:55 +0100

    Extract loadGraphFromFile method and refactor code

--- data-collector/src/test/java/com/agh/bsct/datacollector/entities/graph/GraphInitializer.java
+++ data-collector/src/test/java/com/agh/bsct/datacollector/entities/graph/GraphInitializer.java
@@ -13,54 +13,57 @@ class GraphInitializer {
     private Map<String, Graph> filenameToGraph = new HashMap<>();
 
     Graph initGraph(String filename) {
-        Graph graph = filenameToGraph.get(filename);
-
-        if (graph == null) {
-            graph = new Graph();
-
-            Map<GraphNode, List<GraphEdge>> nodeToEdgesIncidenceMap = new HashMap<>();
-
-            BufferedReader reader;
-            try {
-                reader = new BufferedReader(new FileReader(filename));
-                String line = reader.readLine();
-                String linesForOneMapElement = null;
-                while (line != null) {
-
-                    if (line.matches("\\s+")) {
-                        String[] stringIds = linesForOneMapElement.split("\\s+");
-
-                        GraphNode graphNode = null;
-                        List<GraphEdge> graphEdges = new ArrayList<>();
-
-                        for (String stringId : stringIds) {
-                            Long id = Long.valueOf(stringId);
-
-                            if (graphNode == null) {
-                                graphNode = new GraphNode(id, 1);
-                            } else {
-                                graphEdges.add(new GraphEdge(new GraphNode(id, 1), 1));
-                            }
-                        }
-
-                        nodeToEdgesIncidenceMap.put(graphNode, graphEdges);
-
-                    } else {
-                        linesForOneMapElement = (linesForOneMapElement == null) ? line : (linesForOneMapElement + line);
-                    }
-
-                    line = reader.readLine();
-                }
-                reader.close();
-
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-
-            graph.setNodeToEdgesIncidenceMap(nodeToEdgesIncidenceMap);
-
+        if (!filenameToGraph.containsKey(filename)) {
+            var graph = loadGraphFromFile(filename);
             filenameToGraph.put(filename, graph);
         }
+
+        return filenameToGraph.get(filename);
+    }
+
+    private Graph loadGraphFromFile(String filename) {
+        var graph = new Graph();
+
+        Map<GraphNode, List<GraphEdge>> nodeToEdgesIncidenceMap = new HashMap<>();
+
+        BufferedReader reader;
+        try {
+            reader = new BufferedReader(new FileReader(filename));
+            String line = reader.readLine();
+            String linesForOneMapElement = null;
+            while (line != null) {
+
+                if (line.matches("\\s+")) {
+                    String[] stringIds = linesForOneMapElement.split("\\s+");
+
+                    GraphNode graphNode = null;
+                    List<GraphEdge> graphEdges = new ArrayList<>();
+
+                    for (String stringId : stringIds) {
+                        Long id = Long.valueOf(stringId);
+
+                        if (graphNode == null) {
+                            graphNode = new GraphNode(id, 1);
+                        } else {
+                            graphEdges.add(new GraphEdge(new GraphNode(id, 1), 1));
+                        }
+                    }
+
+                    nodeToEdgesIncidenceMap.put(graphNode, graphEdges);
+
+                } else {
+                    linesForOneMapElement = (linesForOneMapElement == null) ? line : (linesForOneMapElement + line);
+                }
+
+                line = reader.readLine();
+            }
+            reader.close();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        graph.setNodeToEdgesIncidenceMap(nodeToEdgesIncidenceMap);
 
         return graph;
     }
