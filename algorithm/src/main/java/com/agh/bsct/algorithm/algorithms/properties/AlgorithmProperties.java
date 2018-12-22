3eba30cf4a26d2532fa5d4482dee26d02a9d4ebb
Artur <kawala.artek@gmail.com>
2018-12-22 10:23:29 +0100

    Validate properties

--- algorithm/src/main/java/com/agh/bsct/algorithm/algorithms/properties/AlgorithmProperties.java
+++ algorithm/src/main/java/com/agh/bsct/algorithm/algorithms/properties/AlgorithmProperties.java
@@ -4,17 +4,22 @@ import lombok.Data;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
+import org.springframework.validation.annotation.Validated;
+
+import javax.validation.constraints.NotNull;
 
 @Data
 @Configuration
 @ConfigurationProperties(prefix = "algorithm-app")
 @PropertySource("classpath:algorithm-app.properties")
+@Validated
 public class AlgorithmProperties {
 
     /**
      * If set to true, saving some calculation values
      * to file will be possible
      */
+    @NotNull
     private Boolean isWritingValuesToFileEnabled;
 
 }
