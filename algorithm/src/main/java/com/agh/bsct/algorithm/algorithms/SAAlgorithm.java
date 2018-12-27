59fae957110b24611e3ccb3c4bb0760a9e2c2e44
ppaula <palcowskapaula@gmail.com>
2018-12-27 23:31:30 +0100

    + change QUEUE_SIZE to 50

--- algorithm/src/main/java/com/agh/bsct/algorithm/algorithms/SAAlgorithm.java
+++ algorithm/src/main/java/com/agh/bsct/algorithm/algorithms/SAAlgorithm.java
@@ -26,7 +26,7 @@ public class SAAlgorithm implements IAlgorithm {
 
     private static final double INITIAL_TEMPERATURE = 500000.0;
     private static final double ALPHA = 0.9999;
-    private static final int QUEUE_SIZE = 100;
+    private static final int QUEUE_SIZE = 50;
 
     private AlgorithmFunctionsService functionsService;
     private CrossingsService crossingsService;
